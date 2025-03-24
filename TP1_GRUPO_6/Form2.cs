f90fe9e6b879c93afa08c931fa9c4848da9a8d63
Nicole <nickybelu@gmail.com>
2025-03-24 16:19:27 -0300

    EJ1: Prueba de validaciones
    Para:
    - Evitar repeticiones en nombres
    - no agregar espacios en blanco

--- TP1_GRUPO_6/Form2.cs
+++ TP1_GRUPO_6/Form2.cs
@@ -21,46 +21,74 @@ namespace TP1_GRUPO_6
         private void buttonAgregar_Click(object sender, EventArgs e)
         {
 
-            string nombre = textBox_nombre.Text.Trim().ToUpper(); // lo que hago aca es igual el texto escrito, con un trim por lo que dijo nicky, y luego lo hago mayusculas
-            string auxNombre = textBox_nombre.Text; // lo que hago aca es crear una auxilar sin modificar nada
-            
-            if(listBox1.Items.Count == 0) // me fijo si lista esta vacia
+            //string nombre = textBox_nombre.Text.Trim().ToUpper(); // lo que hago aca es igual el texto escrito, con un trim por lo que dijo nicky, y luego lo hago mayusculas
+            //string auxNombre = textBox_nombre.Text.Trim(); // lo que hago aca es crear una auxilar sin modificar nada
+
+            //if (listBox1.Items.Count == 0) // me fijo si lista esta vacia
+            //{
+            //    if (string.IsNullOrEmpty(nombre)) // verifico con un string si esta vacio ("")
+            //    {
+            //        MessageBox.Show("Debe ingresar un nombre.", "Atencion");
+            //    }
+            //    else
+            //    {
+            //        listBox1.Items.Add(auxNombre); // agrego uno a la lista, en este caso sin descriminar
+            //        textBox_nombre.Text = String.Empty;
+            //    }
+            //}
+            //else // condicion para cuando ya hay por lo menos un nombre cargado
+            //{
+            //    bool sonIguales = false; // bandera
+
+            //    for (int i = 0; i < listBox1.Items.Count; i++) // lo que hago aca  es recorrer la lista en busqueda de algun item igual
+            //    {
+            //        if (nombre == listBox1.Items[i].ToString().ToUpper()) // agarro el item y lo comvierto a string y lo hago mayus
+            //        {
+            //            sonIguales = true;
+            //            break; // si son iguales corto ciclo
+            //        }
+
+            //        sonIguales = false;
+            //    }
+
+            //    if (sonIguales) // pregunto
+            //    {
+            //        MessageBox.Show("No se permite ingresar dos nombres iguales", "Atencion");
+            //    }
+            //    else
+            //    {
+            //        listBox1.Items.Add(auxNombre); // agrego el auxnombre
+            //        textBox_nombre.Text = String.Empty;
+            //    }
+
+            //}
+
+            if (textBox_nombre.Text.Trim() != "")
             {
-                if (string.IsNullOrEmpty(nombre)) // verifico con un string si esta vacio ("")
+                bool existe = false;
+
+                foreach (string item in listBox1.Items)
                 {
-                    MessageBox.Show("Debe ingresar un nombre.", "Atencion");
+                    if (textBox_nombre.Text.ToUpper().Trim() == item.ToUpper())
+                    {
+                        existe = true;
+                        break;
+                    }
+                }
+
+                if (existe)
+                {
+                    MessageBox.Show("Este nombre ya fue ingresado.", "Atención");
                 }
                 else
                 {
-                    listBox1.Items.Add(auxNombre); // agrego uno a la lista, en este caso sin descriminar
+                    listBox1.Items.Add(textBox_nombre.Text.Trim());
                     textBox_nombre.Text = String.Empty;
                 }
             }
-            else // condicion para cuando ya hay por lo menos un nombre cargado
+            else
             {
-                bool sonIguales = false; // bandera
-
-                for (int i = 0; i < listBox1.Items.Count; i++) // lo que hago aca  es recorrer la lista en busqueda de algun item igual
-                {
-                    if (nombre == listBox1.Items[i].ToString().ToUpper()) // agarro el item y lo comvierto a string y lo hago mayus
-                    {
-                        sonIguales = true;
-                        break; // si son iguales corto ciclo
-                    }
-
-                    sonIguales = false;
-                }
-
-                if (sonIguales) // pregunto
-                {
-                    MessageBox.Show("No se permite ingresar dos nombres iguales", "Atencion");
-                }
-                else
-                {
-                    listBox1.Items.Add(auxNombre); // agrego el auxnombre
-                    textBox_nombre.Text = String.Empty;
-                }
-
+                MessageBox.Show("Debe ingresar un nombre", "Atención");
             }
 
         }
