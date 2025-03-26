2e9d242776ef15bf079777d9e85df45a868cf004
lunl4nd <ludesantosherrero@gmail.com>
2025-03-26 11:06:46 -0300

    botón femenino/masculino

--- TP1_GRUPO_6/Ejercicio3.Designer.cs
+++ TP1_GRUPO_6/Ejercicio3.Designer.cs
@@ -28,21 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.grpSexo = new System.Windows.Forms.GroupBox();
+            this.FemBtn = new System.Windows.Forms.RadioButton();
+            this.MascBtn = new System.Windows.Forms.RadioButton();
+            this.grpSexo.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // grpSexo
+            // 
+            this.grpSexo.Controls.Add(this.MascBtn);
+            this.grpSexo.Controls.Add(this.FemBtn);
+            this.grpSexo.Location = new System.Drawing.Point(91, 37);
+            this.grpSexo.Name = "grpSexo";
+            this.grpSexo.Size = new System.Drawing.Size(197, 129);
+            this.grpSexo.TabIndex = 0;
+            this.grpSexo.TabStop = false;
+            this.grpSexo.Text = "Sexo";
+            // 
+            // FemBtn
+            // 
+            this.FemBtn.AutoSize = true;
+            this.FemBtn.Checked = true;
+            this.FemBtn.Location = new System.Drawing.Point(26, 46);
+            this.FemBtn.Name = "FemBtn";
+            this.FemBtn.Size = new System.Drawing.Size(88, 20);
+            this.FemBtn.TabIndex = 0;
+            this.FemBtn.TabStop = true;
+            this.FemBtn.Text = "Femenino";
+            this.FemBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.FemBtn.UseVisualStyleBackColor = true;
+            // 
+            // MascBtn
+            // 
+            this.MascBtn.AutoSize = true;
+            this.MascBtn.Location = new System.Drawing.Point(26, 72);
+            this.MascBtn.Name = "MascBtn";
+            this.MascBtn.Size = new System.Drawing.Size(89, 20);
+            this.MascBtn.TabIndex = 1;
+            this.MascBtn.Text = "Masculino";
+            this.MascBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.MascBtn.UseVisualStyleBackColor = true;
             // 
             // Ejercicio3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.grpSexo);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Ejercicio3";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ejercicio3";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Ejercicio3_FormClosed);
+            this.grpSexo.ResumeLayout(false);
+            this.grpSexo.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grpSexo;
+        private System.Windows.Forms.RadioButton MascBtn;
+        private System.Windows.Forms.RadioButton FemBtn;
     }
 }
